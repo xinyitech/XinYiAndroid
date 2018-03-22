d9159c29a35f064e6abbb1150a0b04b4cc679ba7
zhiren.zhang <915924056@qq.com>
2018-03-22 15:04:58 +0800

    ff

--- app/src/main/java/com/xinyiandroid/application/BaseApplication.java
+++ app/src/main/java/com/xinyiandroid/application/BaseApplication.java
@@ -2,12 +2,12 @@ package com.xinyiandroid.application;
 
 
 import com.blankj.utilcode.util.Utils;
-import com.xymaplibrary.application.XinYiMapApplication;
+import com.xylibrary.application.XinYiApplication;
 
 /**
  * Created by jiajun.wang on 2018/3/19.
  */
-public class BaseApplication extends XinYiMapApplication {
+public class BaseApplication extends XinYiApplication {
 
     public static BaseApplication mBaseApplication;
     @Override
